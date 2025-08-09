0000af6594c6a4ada9c0786cda0e83741c99958c
Dwait Pandhi <dwait.pandhi@gmail.com>
2025-08-09 19:40:55 -0400

    make buttons bigger and add eraser

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -245,7 +245,7 @@
     }}
     aria-label="Small brush"
   >
-    <svg viewBox="0 0 6 6" class="w-4">
+    <svg viewBox="0 0 6 6" class="w-8">
       <circle cx="3" cy="3" r="1" fill="black" />
     </svg>
   </button>
@@ -256,7 +256,7 @@
     }}
     aria-label="Medium brush"
   >
-    <svg viewBox="0 0 6 6" class="w-4">
+    <svg viewBox="0 0 6 6" class="w-8">
       <circle cx="3" cy="3" r="2" fill="black" />
     </svg>
   </button>
@@ -267,7 +267,7 @@
     }}
     aria-label="Large brush"
   >
-    <svg viewBox="0 0 6 6" class="w-4">
+    <svg viewBox="0 0 6 6" class="w-8">
       <circle cx="3" cy="3" r="3" fill="black" />
     </svg>
   </button>
@@ -278,9 +278,9 @@
   }}
   aria-label="Erase"
 >
-  <svg viewBox="0 0 6 6" class="w-4">
-    <circle cx="3" cy="3" r="3" stroke="black" fill="white" />
-  </svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-eraser-fill" viewBox="0 0 16 16">
+  <path d="M8.086 2.207a2 2 0 0 1 2.828 0l3.879 3.879a2 2 0 0 1 0 2.828l-5.5 5.5A2 2 0 0 1 7.879 15H5.12a2 2 0 0 1-1.414-.586l-2.5-2.5a2 2 0 0 1 0-2.828zm.66 11.34L3.453 8.254 1.914 9.793a1 1 0 0 0 0 1.414l2.5 2.5a1 1 0 0 0 .707.293H7.88a1 1 0 0 0 .707-.293z"/>
+</svg>
 </button>
 
 <style>
